5b18f7a664fdfad0aa4885633d1c4e8f1405f721
anikinjura <anikinjura@mail.ru>
2024-08-31 14:08:36 +0300

    Добавил в тест reference_books проверку дополниельных методов

--- reference_books/tests.py
+++ reference_books/tests.py
@@ -3,10 +3,11 @@
 from django.contrib.auth.models import User
 from django.test import TestCase, Client
 from django.urls import reverse
-from .models import Agent, PickupPoint, Employee
+from .models import Agent, PickupPoint, Employee, AccountingPeriod
 from datetime import date
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import Permission
+from django.utils import timezone
 
 class AgentModelTest(TestCase):
     """
@@ -17,12 +18,22 @@ class AgentModelTest(TestCase):
     def setUp(self):
         """
         Устанавливает начальные данные для тестов.
-        Создает тестового агента.
+        Создает тестового агента и несколько пунктов выдачи.
         """
         self.agent = Agent.objects.create(
             name="Test Agent", 
             email="test@example.com", 
             phone_number="1234567890"
+        )
+        self.pickup_point_1 = PickupPoint.objects.create(
+            name="Pickup Point 1", 
+            address="123 Test St", 
+            agent=self.agent
+        )
+        self.pickup_point_2 = PickupPoint.objects.create(
+            name="Pickup Point 2", 
+            address="456 Test Ave", 
+            agent=self.agent
         )
 
     def test_agent_creation(self):
@@ -40,6 +51,15 @@ class AgentModelTest(TestCase):
         Убеждается, что метод __str__ возвращает правильное значение.
         """
         self.assertEqual(str(self.agent), "Test Agent")
+
+    def test_agent_get_pickup_points(self):
+        """
+        Проверяет метод get_pickup_points.
+        Убеждается, что метод возвращает правильный набор пунктов выдачи.
+        """
+        pickup_points = self.agent.get_pickup_points()
+        self.assertIn(self.pickup_point_1, pickup_points)
+        self.assertIn(self.pickup_point_2, pickup_points)
 
 
 class PickupPointModelTest(TestCase):
@@ -98,16 +118,25 @@ class EmployeeModelTest(TestCase):
             email="agent@example.com", 
             phone_number="1234567890"
         )
+        self.pickup_point = PickupPoint.objects.create(
+            name="Pickup Point 1", 
+            address="123 Test St", 
+            agent=self.agent
+        )
         self.employee = Employee.objects.create(
             user=self.user,
             first_name="John",
+            middle_name="Middle",
             last_name="Doe",
             email="john.doe@example.com",
             phone_number="1234567890",
             date_of_birth="1990-01-01",
             date_of_hire=date.today(),
             position="Manager",
-            agent=self.agent
+            role="manager",
+            agent=self.agent,
+            default_pickup_point=self.pickup_point,
+            is_active=True
         )
 
     def test_employee_creation(self):
@@ -116,114 +145,91 @@ class EmployeeModelTest(TestCase):
         Убеждается, что созданный сотрудник имеет правильные значения атрибутов.
         """
         self.assertEqual(self.employee.first_name, "John")
+        self.assertEqual(self.employee.middle_name, "Middle")
         self.assertEqual(self.employee.last_name, "Doe")
         self.assertEqual(self.employee.email, "john.doe@example.com")
         self.assertEqual(self.employee.agent, self.agent)
+        self.assertEqual(self.employee.default_pickup_point, self.pickup_point)
 
     def test_employee_str_method(self):
         """
         Проверяет строковое представление сотрудника.
         Убеждается, что метод __str__ возвращает правильное значение.
         """
-        self.assertEqual(str(self.employee), "John  Doe - Manager")
+        self.assertEqual(str(self.employee), "John Middle Doe - Manager")
+
+    def test_employee_get_full_name(self):
+        """
+        Проверяет метод get_full_name.
+        Убеждается, что метод возвращает правильное полное имя сотрудника.
+        """
+        full_name = self.employee.get_full_name()
+        self.assertEqual(full_name, "Doe John Middle")
+
+    def test_employee_deactivate(self):
+        """
+        Проверяет метод deactivate.
+        Убеждается, что метод корректно деактивирует сотрудника.
+        """
+        self.employee.deactivate()
+        self.assertFalse(self.employee.is_active)
 
 
-class AgentAPITest(TestCase):
+class AccountingPeriodModelTest(TestCase):
     """
-    Тесты для API методов модели Agent.
-    Проверяет корректность работы API для модели Agent.
-    """
-
-    def setUp(self):
-        """
-        Устанавливает начальные данные для тестов API.
-        Создает тестового пользователя, токен, назначает разрешения и создает агента.
-        """
-        self.user = User.objects.create_user(username='testuser', password='password')
-        self.token = Token.objects.create(user=self.user)
-        self.client = Client(HTTP_AUTHORIZATION='Token ' + self.token.key)
-
-        # Добавляем необходимые разрешения пользователю
-        manage_permission = Permission.objects.get(codename='manage_employees')
-        view_permission = Permission.objects.get(codename='view_personal_data')
-        self.user.user_permissions.add(manage_permission, view_permission)
-
-        # Создаем тестового агента
-        self.agent = Agent.objects.create(
-            name="Test Agent", 
-            email="test.agent@example.com", 
-            phone_number="1234567890"
-        )
-
-    def test_agent_list(self):
-        """
-        Проверяет получение списка агентов через API.
-        Убеждается, что запрос на получение списка агентов возвращает статус 200 и содержит данные агента.
-        """
-        response = self.client.get(reverse('reference_books_api:agent-list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, self.agent.name)
-
-    def test_agent_detail(self):
-        """
-        Проверяет получение деталей агента через API.
-        Убеждается, что запрос на получение деталей агента возвращает статус 200 и содержит данные агента.
-        """
-        response = self.client.get(reverse('reference_books_api:agent-detail', args=[self.agent.pk]))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, self.agent.email)
-
-
-class EmployeeAPITest(TestCase):
-    """
-    Тесты для API методов модели Employee.
-    Проверяет корректность работы API для модели Employee.
+    Тесты для модели AccountingPeriod.
+    Проверяет корректность создания и работы методов модели AccountingPeriod.
     """
 
     def setUp(self):
         """
-        Устанавливает начальные данные для тестов API.
-        Создает тестового пользователя, токен, назначает разрешения и создает агента и сотрудника.
+        Устанавливает начальные данные для тестов.
+        Создает тестового агента и учетный период.
         """
-        self.user = User.objects.create_user(username='testuser', password='password')
-        self.token = Token.objects.create(user=self.user)
-        self.client = Client(HTTP_AUTHORIZATION='Token ' + self.token.key)
-
-        # Добавляем необходимые разрешения пользователю
-        manage_permission = Permission.objects.get(codename='manage_employees')
-        view_permission = Permission.objects.get(codename='view_personal_data')
-        self.user.user_permissions.add(manage_permission, view_permission)
-
-        # Создаем тестового агента и сотрудника
         self.agent = Agent.objects.create(
             name="Test Agent", 
-            email="test.agent@example.com", 
+            email="agent@example.com", 
             phone_number="1234567890"
         )
-        self.employee = Employee.objects.create(
-            user=self.user,
-            first_name="John",
-            last_name="Doe",
-            email="john.doe@example.com",
-            date_of_hire=date.today(),
-            position="Manager",
-            agent=self.agent
+        self.accounting_period = AccountingPeriod.objects.create(
+            agent=self.agent,
+            start_date=date(2023, 1, 1),
+            end_date=date(2023, 12, 31)
         )
 
-    def test_employee_list(self):
+    def test_accounting_period_creation(self):
         """
-        Проверяет получение списка сотрудников через API.
-        Убеждается, что запрос на получение списка сотрудников возвращает статус 200 и содержит данные сотрудника.
+        Проверяет создание учетного периода.
+        Убеждается, что созданный учетный период имеет правильные значения атрибутов.
         """
-        response = self.client.get(reverse('reference_books_api:employee-list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, self.employee.email)
+        self.assertEqual(self.accounting_period.agent, self.agent)
+        self.assertEqual(self.accounting_period.start_date, date(2023, 1, 1))
+        self.assertEqual(self.accounting_period.end_date, date(2023, 12, 31))
 
-    def test_employee_detail(self):
+    def test_accounting_period_str_method(self):
         """
-        Проверяет получение деталей сотрудника через API.
-        Убеждается, что запрос на получение деталей сотрудника возвращает статус 200 и содержит данные сотрудника.
+        Проверяет строковое представление учетного периода.
+        Убеждается, что метод __str__ возвращает правильное значение.
         """
-        response = self.client.get(reverse('reference_books_api:employee-detail', args=[self.employee.pk]))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, self.employee.first_name)
+        self.assertEqual(str(self.accounting_period), "Учетный период: 2023-01-01 - 2023-12-31 (Test Agent)")
+
+    def test_accounting_period_is_active(self):
+        """
+        Проверяет метод is_active.
+        Убеждается, что метод корректно определяет активность учетного периода.
+        """
+        today = timezone.now().date()
+        active_period = AccountingPeriod.objects.create(
+            agent=self.agent,
+            start_date=today,
+            end_date=today
+        )
+        self.assertTrue(active_period.is_active())
+
+        past_period = AccountingPeriod.objects.create(
+            agent=self.agent,
+            start_date=date(2022, 1, 1),
+            end_date=date(2022, 12, 31)
+        )
+        self.assertFalse(past_period.is_active())
+
